b4daf99edf7c8b4684dc16111d0897cea74cd7f4
Имран <imran@MacBook-Pro-Imran.local>
2024-12-08 14:11:42 +0300

    sj

---
+++ app/(client)/posts/not-found.tsx
@@ -0,0 +1,16 @@
+﻿import Header from '@/app/components/Header'
+import Link from 'next/link'
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+        <Header title='404 - Page Not Found' />
+        <div>
+            <Link href="/">Return Home</Link>
+        </div>
+    </div>
+  )
+}
+
+export default NotFound
